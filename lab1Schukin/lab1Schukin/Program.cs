4895ab0374cd1b7cbb6754cba145c6d202294df7
A1wayssswannaf1y <144476659+A1wayssswannaf1y@users.noreply.github.com>
2023-09-08 19:53:02 +0300

    Add files via upload

---
+++ lab1Schukin/lab1Schukin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1Schukin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                sbyte a = -100;
+            sbyte b = Math.Pow(Math.Abs(Math.Cos(x)));
+
+
+        }
+    }
+}
